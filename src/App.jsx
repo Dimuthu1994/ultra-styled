a4f4fdca30baf365bdae17235f802aa91595cfce
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-09-06 11:21:10 +0530

    build navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import React from "react";
+import GlobalStyle from "./globalStyles";
+import { Navbar } from "./components";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Navbar />
+    </BrowserRouter>
+  );
 }
 
 export default App;
