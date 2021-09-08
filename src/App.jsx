7244ca101ada187d4a3f149108d9e6411ba3aacc
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-09-08 07:48:53 +0530

    build info page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import GlobalStyle from "./globalStyles";
 import { Navbar } from "./components";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/HomePage/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
     </BrowserRouter>
   );
 }
---
+++ src/pages/HomePage/Data.jsx
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  lightBg: false,
+  primary: true,
+  lightBg: false,
+  imgStart: "",
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  description:
+    "We help business owners increase their revenue.Our team of unique specialist can help you achieve your business goals.",
+  headline: "Lead Generation Specialist for Online Businesses",
+  lightText: true,
+  topLine: "Marketing Agency",
+  img: "../../images/svg-1.svg",
+  alt: "Image",
+  start: "",
+};
